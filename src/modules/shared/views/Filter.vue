ff6301a2d9fcda62af7a7fb21fcd7a5daaac4f7e
Guilherme Dornelles <guilherme.dornelles@nubo.ai>
2022-04-18 18:41:18 -0300

    Adicionando arquivo da view de filtros

---
+++ src/modules/shared/views/Filter.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <div class="title ml-2 mt-4">
+      <div>Categoria</div>
+      <v-row></v-row>
+    </div>
+
+    <div class="title ml-2 mt-4">
+      <div>Tipo</div>
+      <v-row></v-row>
+    </div>
+
+    <div class="title ml-2 mt-4">
+      <div>Status</div>
+      <v-row></v-row>
+    </div>
+
+    <div class="title ml-2 mt-4">
+      <div>Data</div>
+      <v-row></v-row>
+    </div>
+
+    <div>
+      <Button
+        class="justify-center align-center"
+        title="Aplicar Filtros"
+        color="primary"
+        elevation="2"
+        @click="$router.push('/home')"
+      ></Button>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Vue from "vue";
+import Button from "../components/Button.vue";
+
+export default Vue.extend({
+  components: { Button },
+  data: () => ({
+    filtro: "Filtro de pesquisa",
+  }),
+  async mounted() {
+    this.$root.showToolbar("FILTRO DE PESQUISA");
+  },
+});
+</script>
+
+<style scoped>
+.title {
+  font-weight: bold;
+}
+.a-fab {
+  max-width: 120px;
+  position: fixed;
+  bottom: 12px;
+}
+</style>
