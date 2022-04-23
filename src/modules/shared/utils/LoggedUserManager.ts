a7745fd04cb052dd537ab0b72e1577580481359e
Vinicius Bazanella <vbazanella@nelogica.com.br>
2022-04-23 10:57:35 -0300

    Implementa gerenciamento da rota Home dependendo do usuário

---
+++ src/modules/shared/utils/LoggedUserManager.ts
@@ -0,0 +1,10 @@
+import { UserEntity } from "../models/UserEntity";
+
+export function saveUserData(userData: UserEntity): void {
+  localStorage.setItem("current-logged-user", JSON.stringify(userData));
+}
+
+export function getUserData(): UserEntity {
+  const storedUserPaylod = localStorage.getItem("current-logged-user") ?? "{}";
+  return JSON.parse(storedUserPaylod) as UserEntity;
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,7 @@ import SplashScreen from "../modules/shared/views/SplashScreen.vue";
 import EditNecessity from "../modules/institution/views/EditNecessity.vue";
 import RegisterNecessity from "../modules/institution/views/RegisterNecessity.vue";
 import Necessities from "../modules/shared/views/Necessities.vue";
+import Home from "../modules/shared/views/Home.vue";
 import { isAuthenticated } from "@/modules/shared/utils/AuthenticationManager";
 
 Vue.use(VueRouter);
@@ -23,8 +24,8 @@ const routes: Array<RouteConfig> = [
   },
   {
     path: "/home/",
-    name: "Necessities",
-    component: Necessities,
+    name: "Home",
+    component: Home,
   },
   {
     path: "/necessity/:id",
