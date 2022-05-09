e23836b1cdaf41fd9cb7aa96ae0a0a72804591d3
unknown <henrique.zwetsch@edu.pucrs.br>
2022-05-09 00:32:19 -0300

    Adicionado serviço de criar doação apartir de necessidade.

--- src/modules/donationService.ts
+++ src/modules/donationService.ts
@@ -15,3 +15,19 @@ export function createDonation(donation: RequestEntity): Promise<void> {
       return Promise.reject(error);
     });
 }
+export function createDonationFromNecessity(
+  donation: RequestEntity
+): Promise<void> {
+  return HTTP.post("/pedidos", {
+    assunto: donation.title,
+    descricao: donation.description,
+    categoria: donation.category,
+    subcategoria: donation.subcategory,
+  })
+    .then(() => {
+      return Promise.resolve();
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
+}
