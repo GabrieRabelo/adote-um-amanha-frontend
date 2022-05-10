468c82192566720038c27876e704eff01b505633
guilherme.dornelles <guilherme.dornelles@edu.pucrs.br>
2022-05-09 22:39:09 -0300

    Separando testes de diferentes fluxos por pasta

--- cypress/integration/Instituition/login.spec.js
+++ cypress/integration/Instituition/login.spec.js
@@ -2,10 +2,10 @@ describe("Login", () => {
   it("Logar", () => {
     cy.visit("/auth");
     cy.get("input[type='email']", { timeout: Cypress.env("timeOut") }).type(
-      Cypress.env("email")
+      Cypress.env("emailHouse")
     );
     cy.get("input[type='password']", { timeout: Cypress.env("timeOut") }).type(
-      Cypress.env("senha")
+      Cypress.env("password")
     );
     cy.get(
       "button[class='a-button v-application px-9 py-5 v-btn v-btn--has-bg v-btn--rounded theme--light elevation-0 v-size--default primary']",
