545693b71f17fe0dde57759b5ec67405f4692557
Cleyson Braga de Oliveira <cleyson.oliveira@edu.pucrs.br>
2022-05-11 00:13:44 +0000

    Fix sftp username variable

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 ECHO OFF
 
 ssh -i "C:\Users\%USERNAME%\.ssh\adote.pem" ec2-user@ec2-18-118-226-194.us-east-2.compute.amazonaws.com "bash ./scripts/backup_frontend.sh"
-sftp -b "deploy.sftp" -i "C:\Users\%1\.ssh\adote.pem" -P 22 ec2-user@ec2-18-118-226-194.us-east-2.compute.amazonaws.com
+sftp -b "deploy.sftp" -i "C:\Users\%USERNAME%\.ssh\adote.pem" -P 22 ec2-user@ec2-18-118-226-194.us-east-2.compute.amazonaws.com
 ssh -i "C:\Users\%USERNAME%\.ssh\adote.pem" ec2-user@ec2-18-118-226-194.us-east-2.compute.amazonaws.com "bash ./scripts/deploy_frontend.sh"
