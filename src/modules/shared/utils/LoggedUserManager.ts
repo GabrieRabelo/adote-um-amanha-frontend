9b74068da8af622a3627e601855a6f11ed575c48
Lucas Cardoso <lucas.gavirachi@edu.pucrs.br>
2022-05-23 21:54:18 -0300

    Finaliza listagem necessidades admin

--- src/modules/shared/utils/LoggedUserManager.ts
+++ src/modules/shared/utils/LoggedUserManager.ts
@@ -1,4 +1,5 @@
 import { UserEntity } from "../models/UserEntity";
+import { UserRole } from "../enums/UserRole";
 
 export function saveUserData(userData: Partial<UserEntity>): void {
   localStorage.setItem("current-logged-user", JSON.stringify(userData));
@@ -7,4 +8,16 @@ export function saveUserData(userData: Partial<UserEntity>): void {
 export function getUserData(): UserEntity {
   const storedUserPaylod = localStorage.getItem("current-logged-user") ?? "{}";
   return JSON.parse(storedUserPaylod) as UserEntity;
+}
+
+export function isUserInstitution(): boolean {
+  return getUserData().role == UserRole.institution;
+}
+
+export function isUserDonator(): boolean {
+  return getUserData().role == UserRole.donator;
+}
+
+export function isUserAdmin(): boolean {
+  return getUserData().role == UserRole.admin;
 }
