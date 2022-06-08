bf1347b9e7b8269eef3303dcc5b5d9edb47cebca
Vinicius Bazanella <v.bazanella@edu.pucrs.br>
2022-06-08 20:51:25 +0000

    US29, US31 - Listagem e filtro de doadores

---
+++ src/modules/shared/components/Slider.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-if="title" class="slider__title">
+      {{ title }}
+    </div>
+    <v-slider v-bind="$attrs" v-on="$listeners">
+      <template v-if="prependText" slot="prepend">{{ prependText }}</template>
+      <template v-if="appendText" slot="append">{{ appendText }}</template>
+    </v-slider>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    prependText: String,
+    appendText: String,
+    title: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.slider__title {
+  font-weight: 600;
+}
+</style>
