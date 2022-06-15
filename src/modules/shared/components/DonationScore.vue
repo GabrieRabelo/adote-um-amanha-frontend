12af0b0ef43a74065993bcf175585beee21a84d9
vitoria.hahn <vitoria.ebeling@edu.pucrs.br>
2022-06-14 23:41:30 -0300

    Inicia Componente.

---
+++ src/modules/shared/components/DonationScore.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-row class="mt-7"><v-divider /></v-row>
+    <v-row class="mt-7">Doações</v-row>
+    <v-row>
+      <v-card max-width="150">
+        <v-card-title class="v-card__title">APROVADAS</v-card-title>
+        <v-card-text>metodo1</v-card-text>
+      </v-card>
+      <v-card max-width="150">
+        <v-card-title>RECUSADAS</v-card-title>
+        <v-card-text>metodo2</v-card-text>
+      </v-card>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.v-card__title {
+  font-size: 100%;
+}
+</style>
