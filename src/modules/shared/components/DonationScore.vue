9b7f3c5bb4351486cef3c4c879ce3ed17127d293
vitoria.hahn <vitoria.ebeling@edu.pucrs.br>
2022-06-15 19:00:53 -0300

    Outline Card e centraliza texto do card.

--- src/modules/shared/components/DonationScore.vue
+++ src/modules/shared/components/DonationScore.vue
@@ -3,13 +3,18 @@
     <v-row class="mt-7"><v-divider /></v-row>
     <v-row class="mt-7">Doações</v-row>
     <v-row class="justify-center mt-10">
-      <v-card class="a-card-aprovado mr-5" max-width="150" elevation="0">
+      <v-card
+        class="a-card-aprovado mr-5"
+        max-width="150"
+        elevation="0"
+        outlined
+      >
         <v-card-title>APROVADAS</v-card-title>
-        <v-card-text>{{ approved }}</v-card-text>
+        <v-card-text class="a-card-text">{{ approved }}</v-card-text>
       </v-card>
-      <v-card class="a-card-rejeitado" max-width="150" elevation="0">
+      <v-card class="a-card-rejeitado" max-width="150" elevation="0" outlined>
         <v-card-title>RECUSADAS</v-card-title>
-        <v-card-text>{{ refused }}</v-card-text>
+        <v-card-text class="a-card-text">{{ refused }}</v-card-text>
       </v-card>
     </v-row>
   </div>
@@ -26,8 +31,9 @@ export default Vue.extend({
 </script>
 
 <style>
-.a-card-aprovado {
-  box-shadow: none;
+.a-card-text {
+  text-align: center;
+  font-size: 20px;
 }
 .v-card__title {
   font-size: 100%;
