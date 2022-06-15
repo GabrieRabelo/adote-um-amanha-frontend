ddec89e88aae429ae0f74e7c6998401dce8cf270
vitoria.hahn <vitoria.ebeling@edu.pucrs.br>
2022-06-15 18:52:02 -0300

    Centraliza card e adiciona prop de doacoes aprovadas e rejeitadas.

--- src/modules/shared/components/DonationScore.vue
+++ src/modules/shared/components/DonationScore.vue
@@ -2,24 +2,33 @@
   <div>
     <v-row class="mt-7"><v-divider /></v-row>
     <v-row class="mt-7">Doações</v-row>
-    <v-row>
-      <v-card max-width="150">
-        <v-card-title class="v-card__title">APROVADAS</v-card-title>
-        <v-card-text>metodo1</v-card-text>
+    <v-row class="justify-center mt-10">
+      <v-card class="a-card-aprovado mr-5" max-width="150" elevation="0">
+        <v-card-title>APROVADAS</v-card-title>
+        <v-card-text>{{ approved }}</v-card-text>
       </v-card>
-      <v-card max-width="150">
+      <v-card class="a-card-rejeitado" max-width="150" elevation="0">
         <v-card-title>RECUSADAS</v-card-title>
-        <v-card-text>metodo2</v-card-text>
+        <v-card-text>{{ refused }}</v-card-text>
       </v-card>
     </v-row>
   </div>
 </template>
 
 <script>
-export default {};
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    approved: Number,
+    refused: Number,
+  },
+});
 </script>
 
 <style>
+.a-card-aprovado {
+  box-shadow: none;
+}
 .v-card__title {
   font-size: 100%;
 }
