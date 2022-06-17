1bbf9b38a765029f5af99541117ad58437859145
Vinicius Bazanella <vbazanella@nelogica.com.br>
2022-06-17 20:15:49 -0300

    Resolve erro de TS

--- src/modules/donator/models/DonatorEntity.ts
+++ src/modules/donator/models/DonatorEntity.ts
@@ -4,21 +4,17 @@ import { UserRole } from "@/modules/shared/enums/UserRole";
 
 export default class DonatorEntity extends UserEntity {
   constructor(
-   public id: number,
-   public name: string,
-   public phone: string,
-   public email: string,
-   public site: string,
-   public cpf_cnpj: string,
-   public addressDTO: AddressDTO,
-   public password?: string
+    public id: number,
+    public name: string,
+    public phone: string,
+    public email: string,
+    public site: string,
+    public cpf_cnpj: string,
+    public addressDTO: AddressDTO,
+    public password?: string
   ) {
-    super(id, name, phone, email, site, addressDTO, UserRole.donator);
-    this.password = password
-    this.cpf_cnpj = cpf_cnpj
-    
-
+    super(id, name, phone, email, addressDTO, UserRole.donator, site);
+    this.password = password;
+    this.cpf_cnpj = cpf_cnpj;
   }
 }
-
-
