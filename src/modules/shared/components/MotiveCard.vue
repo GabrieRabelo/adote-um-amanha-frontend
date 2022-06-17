f20cf5fd414d3c336963a2681b8b875c38bfd502
vitoria.hahn <vitoria.ebeling@edu.pucrs.br>
2022-06-17 19:16:50 -0300

    Inicia componente do motivo da recusa.

---
+++ src/modules/shared/components/MotiveCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title> Motivo </v-card-title>
+      <v-card-text>
+        {{ motive }}
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    motive: String,
+  },
+});
+</script>
+
+<style></style>
