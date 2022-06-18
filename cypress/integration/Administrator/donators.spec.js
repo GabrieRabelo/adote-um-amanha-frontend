24c8914d9b5fe5325517d4dd9e37299ccacfde6d
Clara D'Ávila <claracunhadavila@gmail.com>
2022-06-18 20:30:44 -0300

    Testes de perfil de doadores

---
+++ cypress/integration/Administrator/donators.spec.js
@@ -0,0 +1,42 @@
+describe("Doadores", () => {
+  beforeEach(() => {
+    cy.visit("/auth");
+    cy.get("input[type='email']", { timeout: Cypress.env("timeOut") }).type(
+      Cypress.env("emailAdmin")
+    );
+    cy.get("input[type='password']", { timeout: Cypress.env("timeOut") }).type(
+      Cypress.env("password")
+    );
+    cy.contains('[class="a-button--title"]', "Entrar", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.url().should("include", "/home", {
+      timeout: Cypress.env("timeOut"),
+    });
+  });
+  it("Open donator card and click back button", () => {
+    cy.contains('[class="a-button--title"]', "Doadores", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.contains('[class="card__title"]', "Gabriel", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.contains('[class="a-button--title"]', "Voltar", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+  });
+  it("Open donator card and click back arrow button", () => {
+    cy.contains('[class="a-button--title"]', "Doadores", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.contains('[class="card__title"]', "Gabriel", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.get(
+      "button[class='v-app-bar__nav-icon v-btn v-btn--icon v-btn--round theme--light v-size--default']",
+      {
+        timeout: Cypress.env("timeOut"),
+      }
+    ).click();
+  });
+});
