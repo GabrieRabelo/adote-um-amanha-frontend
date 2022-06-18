33e431d81fc77d6c0bd8d340630f38966534e1d8
Clara D'Ávila <claracunhadavila@gmail.com>
2022-06-18 19:48:49 -0300

    Testes dos filtros de necessidade

---
+++ cypress/integration/Donator/filterNecessity.spec.js
@@ -0,0 +1,71 @@
+describe("Filter Necessity", () => {
+  beforeEach(() => {
+    cy.visit("/auth");
+    cy.get("input[type='email']", { timeout: Cypress.env("timeOut") }).type(
+      Cypress.env("emailDonator")
+    );
+    cy.get("input[type='password']", { timeout: Cypress.env("timeOut") }).type(
+      Cypress.env("password")
+    );
+    cy.get(
+      "button[class='a-button v-application px-9 py-5 v-btn v-btn--has-bg v-btn--rounded theme--light elevation-0 v-size--default primary']",
+      { timeout: Cypress.env("timeOut") }
+    ).click();
+  });
+
+  it("Filtra por categoria", () => {
+    cy.contains('[class="a-button--title"]', "Necessidades das Casas", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.get(
+      "button[class='v-btn v-btn--is-elevated v-btn--fab v-btn--has-bg v-btn--round theme--light v-size--default primary']",
+      {
+        timeout: Cypress.env("timeOut"),
+      }
+    ).click();
+    cy.contains('[class="v-chip__content"]', "Saúde", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.contains('[class="a-button--title"]', "Aplicar Filtros").click();
+    cy.contains('[class="card__title"]', "Consulta Nutricional").click();
+  });
+
+  it("Filtra por tipo", () => {
+    cy.contains('[class="a-button--title"]', "Necessidades das Casas", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.get(
+      "button[class='v-btn v-btn--is-elevated v-btn--fab v-btn--has-bg v-btn--round theme--light v-size--default primary']",
+      {
+        timeout: Cypress.env("timeOut"),
+      }
+    ).click();
+    cy.contains('[class="v-chip__content"]', "Bens", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.contains('[class="a-button--title"]', "Aplicar Filtros").click();
+    cy.contains('[class="card__title"]', "Leite em pó desnatado").click();
+  });
+
+  it("Filtra por data", () => {
+    cy.contains('[class="a-button--title"]', "Necessidades das Casas", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.get(
+      "button[class='v-btn v-btn--is-elevated v-btn--fab v-btn--has-bg v-btn--round theme--light v-size--default primary']",
+      {
+        timeout: Cypress.env("timeOut"),
+      }
+    ).click();
+    cy.contains('[class="v-select__slot"]', "Data").click();
+    cy.contains('[class="v-list-item__title"]', "6 meses", {
+      timeout: Cypress.env("timeOut"),
+    }).click();
+    cy.contains('[class="a-button--title"]', "Aplicar Filtros").click();
+    cy.contains('[class="card__title"]', "Leite em pó desnatado").click();
+    cy.contains(
+      '[class="a-text ml-3 text-decoration-underline" ]',
+      "Lar Esperança"
+    ).click();
+  });
+});
